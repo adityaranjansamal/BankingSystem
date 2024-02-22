cd19404785272b9a9668d5c453847e79d40ea845
adityaranjansamal <83838764+adityaranjansamal@users.noreply.github.com>
2024-02-22 21:53:13 +0530

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,6 +1,14 @@
 import java.util.*;
 
 /*
+ * Author: Aditya Ranjan Samal
+ * Github ID: adityarnsamal
+ * Telegram ID: adityarnsamal
+ * Compiled On: Fedora Linux 39 Workstation Edition x64_86
+ * Java(c) Version: 11
+ * Font Used: JetBrains Mono
+ */
+/*
  * This is the Login Page For the Banking System.
  * This is the Central Control Unit.
  */
@@ -8,21 +16,61 @@ public class Login {
     static Scanner sc2 = new Scanner(System.in);
     static Scanner stringsc2 = new Scanner(System.in);// String is entered through different scanner object to avoid
                                                       // mismatch
+    static boolean man_auth = false;
+    static boolean cust_auth = false;
 
     public static void whoami() {// This specifies how the login should occur
         while (true) {// Infinite Loop Until Case 3 exit occurs
-            System.out.println("***Login As***");
+            System.out.println("***Access As***");
             System.out.println("1.Manager");
             System.out.println("2.Customer");
             System.out.println("3.Exit");
+            System.out.println();
             int ch = sc2.nextInt();
             switch (ch) {
                 case 1:
-                    System.out.println("Manager Portal Login");// Logging in as Bank Manager
+                    // Opening Portal
+                    System.out.println("Welcome to Manager's Portal");
+                    while (true)// while 0
+                    {
+                        System.out.println("1. Create Manager");
+                        System.out.println("2. Login as Manager");
+                        System.out.println("3. Return to Main Menu");
+                        int man_ch = sc2.nextInt();
+                        switch (man_ch) {
+                            case 1:
+                                Manager.man_data();
+                                break;
+                            case 2:
+                                if (Manager.man_uid != 0L)
+                                    man_auth = true;
+                                break;
+                            case 3:
+                                Login.whoami();
+                                break;
+                            default:
+                                System.out.println("Wrong Choice! Plaese ReEnter");
+                                break;
+                        }
+                        if (man_auth && man_ch == 2)
+                            break;// while 0 terminated!
+                        else {
+                            continue;
+                        }
+                    }
+                    // Portal End
+                    // Manager Login Authentication
+                    System.out.println();
+                    System.out.println("Manager Portal Login");// Logging in as bank Manager
                     while (true) {// while 1
-                        System.out.println("Enter UID");
+                        System.out.println();
+                        System.out.println("Enter UID, Enter -9999 to Exit to Main Menu");
                         long uid = sc2.nextLong();
-                        if (uid == Manager.man_uid) {// checks if uid matches with id stored in database
+                        if (uid == -9999) {
+                            System.out.println("Returning to main menu");
+                            Login.whoami();
+                            return;
+                        } else if (uid == Manager.man_uid) {// checks if uid matches with id stored in database
                             while (true) {// while 2
                                 System.out.println("Enter MPIN");
                                 String mpin = stringsc2.nextLine();
@@ -31,6 +79,7 @@ public class Login {
                                                                                  // value matches with the hash stored
                                                                                  // in the databse or not
                                     System.out.println("Manager Login Success!");
+                                    System.out.println("Welcome Manager " + Manager.man_name + "!");
                                     Manager.menu();
                                     break;// while 2 loop break
                                 } else {
@@ -50,11 +99,48 @@ public class Login {
                     break;// case 1 break
 
                 case 2:
+                    // Opening Portal
+                    System.out.println("Welcome to Customer's Portal");
+                    while (true)// while customer 0
+                    {
+                        System.out.println("1. Create customer account");
+                        System.out.println("2. Login as Customer");
+                        System.out.println("3. Return to Main Menu");
+                        int custom_ch = sc2.nextInt();
+                        switch (custom_ch) {
+                            case 1:
+                                Manager.createuser();
+                                break;
+                            case 2:
+                                if (Manager.head != null)
+                                    cust_auth = true;
+                                break;
+                            case 3:
+                                Login.whoami();
+                                break;
+                            default:
+                                System.out.println("Wrong Choice! Please ReEnter");
+                                break;
+                        }
+                        if (cust_auth && custom_ch == 2)
+                            break;// while customer 0 terminated!
+                        else if (custom_ch == 1)
+                            continue;
+                        else {
+                            System.out.println("No Account present in bank! Please create a Customer account");
+                            continue;
+                        }
+                    }
+                    // Portal End
                     System.out.println("Customer Portal Login");// Logging in as Customer
                     while (true) {// while 1
-                        System.out.println("Enter Account Number");
+                        System.out.println("Enter Account Number. To Return to Main Menu Enter -9999");
                         long uid2 = sc2.nextLong();
-                        if (Customer.findAcc(uid2)) {// checks if uid2 matches with id stored in database
+                        if (uid2 == -9999) {
+                            System.out.println("Returning to Main Menu");
+                            Login.whoami();
+                            return;
+                        } else if (Customer.findAcc(uid2)) {// checks if uid2 matches with id stored in database
                             while (true) {// while 2
                                 System.out.println("Enter MPIN");
                                 String mpin = stringsc2.nextLine();
@@ -63,6 +149,7 @@ public class Login {
                                                       // in the databse or not
                                 if ((Customer.matchPass(Secrets.hashedpin))) {
                                     System.out.println("Customer Login Success!");
+                                    System.out.println("Welcome " + Customer.temp.username + "!");
                                     Customer.menu2();
                                     break;// while 2 break
                                 } else {
@@ -90,8 +177,13 @@ public class Login {
     }
 
     public static void main(String[] args) {
-        Manager.man_data();// calling this function at least once beacuse manager is needed to create
-                           // customer accounts
+        // System.out.println("====WELCOME TO THALA BANK====");
+        faceprint.logo();// Prints the Bank Name
+        // System.out.println("First Run! Create Manager First to invoke any
+        // functions");
+        // Manager.man_data();// calling this function at least once beacuse manager is
+        // needed to create
+        // customer accounts
         Login.whoami();
     }
 }
--- Manager.java
+++ Manager.java
@@ -5,22 +5,27 @@
 import java.util.*;
 
 public class Manager {
-    static Scanner stringsc3 = new Scanner(System.in);// String is entered through different scanner object to avoid
-                                                      // mismatch
-    static Scanner sc3 = new Scanner(System.in);
+    private static Scanner stringsc3 = new Scanner(System.in);// String is entered through different scanner object to
+                                                              // avoid
+    // mismatch
+    private static Scanner sc3 = new Scanner(System.in);
     static long man_uid = 0L;
+    static String man_name;
     static String man_mpin;
     static Node head = null, n = null;
-    static Random random = new Random();
-    static int call = 0;
+    private static int call = 0;
+    private static boolean isInSupport = false;
 
-    public static void menu() {// Manager Available options
-        Manager.man_data();// only invoked once, to create manager for successful run of the program
+    protected static void menu() {// Manager Available options
+        // Manager.man_data();// only invoked once, to create manager for successful run
+        // of the program
         while (true) {
             System.out.println("***Manager Menu***");
             System.out.println("1. Create User Account");
             System.out.println("2. Update Balance of User");
-            System.out.println("3. Logout");
+            System.out.println("3. Display All Accounts Created in Bank");
+            System.out.println("4. Customer's Request Page");
+            System.out.println("5. Logout");
             System.out.println("Enter a choice");
             Manager.cases_M(sc3.nextInt());
             if (call == 1) {// Logout Case
@@ -32,7 +37,7 @@ public class Manager {
 
     }
 
-    public static void cases_M(int choice) {
+    private static void cases_M(int choice) {
         switch (choice) {
             case 1:
                 Manager.createuser();
@@ -41,6 +46,12 @@ public class Manager {
                 Manager.update_balance();
                 break;
             case 3:
+                Manager.display(head);
+                break;
+            case 4:
+                Manager.customer_requests(head);
+                break;
+            case 5:
                 call = 1;
                 break;
             default:
@@ -49,11 +60,13 @@ public class Manager {
         }
     }
 
-    public static void man_data() {// This creates the Manager id and mpin & stores it in a variable. As there is
-                                   // only one manager account therefore a LinkedList is not required
+    protected static void man_data() {// This creates the Manager id and mpin & stores it in a variable. As there is
+                                      // only one manager account therefore a LinkedList is not required
         if (man_uid == 0L) {
             System.out.println("Create Manager ID");
             man_uid = sc3.nextLong();
+            System.out.println("Create Manager Name");
+            man_name = stringsc3.nextLine();
             Secrets.generate_mpin();// creates mpin & encrypts the value
             man_mpin = Secrets.hashedpin;// stores the encrypted value
         } else {
@@ -62,7 +75,7 @@ public class Manager {
         }
     }
 
-    public static void createuser() {// Normal LinkedList for customer account creation
+    protected static void createuser() {// Normal LinkedList for customer account creation
         if (head == null) {
             n = new Node();
             n.next = null;
@@ -70,7 +83,7 @@ public class Manager {
             System.out.println("Enter your Name");
             n.username = stringsc3.nextLine();
             System.out.println("Creating Account Number....");
-            n.accno = random.nextInt(9_00_000) + 1_00_000;// generate no b/n 1,00,000-9,99,999
+            n.accno = RandomIdGenerator.RandomID();// generate no b/n 1,00,000-9,99,999
             System.out.println("Create a MPIN");
             Secrets.generate_mpin();
             n.hash_pin = Secrets.hashedpin;
@@ -82,8 +95,8 @@ public class Manager {
             System.out.println("Enter your Name");
             newnode.username = stringsc3.nextLine();
             System.out.println("Creating Account Number....");
-            newnode.accno = random.nextInt(9_00_000) + 1_00_000;// generate no b/n 1,00,000-9,99,999
-            System.out.println("Create a MPIN");
+            newnode.accno = RandomIdGenerator.RandomID();// generate no b/n 1,00,000-9,99,999
+            // System.out.println("Create a MPIN");
             Secrets.generate_mpin();
             newnode.hash_pin = Secrets.hashedpin;
             Manager.print1(newnode);
@@ -91,7 +104,7 @@ public class Manager {
         }
     }
 
-    public static void update_balance() {// Manager has the authority to modify the balances of user accounts
+    private static void update_balance() {// Manager has the authority to modify the balances of user accounts
         System.out.println("Enter Ac/No");
         long acc = sc3.nextLong();
         boolean isPresent = Customer.findAcc(acc);// searches if the account is present or not
@@ -118,6 +131,121 @@ public class Manager {
         System.out.println("Name:" + k.username);
         System.out.println("Account Number:" + k.accno);
         System.out.println("Balance:" + k.balance);
+    }
+
+    private static void display(Node k) {
+        Node disp = k;
+        if (k == null) {
+            System.out.println(
+                    "No Accounts Present in THALA Bank! Probably First Run\n Create new User Accounts first\n Good Luck!");
+            return;
+        }
+        String str2 = "";
+        System.out.printf("%s%8s%s%8s%s%8s%s", "NAME", "", "A/CN", "", "BAL", "", "REQ");
+        while (disp != null) {// iterates over the Linked List
+            System.out.println();
+            str2 = disp.username;// sets str2 as username
+            System.out.print(str2);// prints str2
+            space_counter.setspaces(str2);// calls the setspaces() to set exact spaces required for formatting
+            str2 = String.valueOf(disp.accno);// sets str2 as account number
+            System.out.print(str2);// prints str2
+            space_counter.setspaces(str2);// calls the setspaces() to set exact spaces required for formatting
+            System.out.print(disp.balance);// prints balance & jumps to the next Line!
+            str2 = String.valueOf(disp.balance);
+            space_counter.setspaces(str2);
+            System.out.println(disp.request);
+            if (!isInSupport)
+                disp = disp.next;
+            else
+                break;
+        }
+        System.out.println();
+    }
+
+    public static void customer_requests(Node k) {
+        System.out.println("Welcome to Customer's Request page!");
+        int broker = 0;
+        isInSupport = true;
+        while (true) {
+            System.out.println("***REQUESTS MENU***");
+            System.out.println("1. List All Requests");
+            System.out.println("2. Address Request");
+            System.out.println("3. Exit Request page!");
+            int reqchoice = sc3.nextInt();
+            switch (reqchoice) {
+                case 1:
+                    Node disp = k;
+                    System.out.println("The Customer requested profiles are listed as follows");
+                    while (disp != null) {
+                        if (disp.request == 1)
+                            Manager.display(disp);
+                        disp = disp.next;
+
+                    }
+                    k = head;
+                    break;
+                case 2:
+                    System.out.println("Enter Account Number of Customer of whom Request will be addressed");
+                    while (true) {
+                        long customer_accn = sc3.nextLong();
+                        if (Customer.findAcc(customer_accn)) {
+                            System.out.println("Account Found!");
+                            System.out.println("Listing Request of:" + Customer.temp.username);
+                            break;
+                        } else {
+                            System.out.println("Account Not Found! Plaese ReEnter");
+                            continue;
+                        }
+                    }
+                    System.out.println("Requesting for changing username to:" + Customer.temp.query);
+                    System.out.println("Showing verification ID");
+                    System.out.println(Customer.temp.upload_file);// Showing Aadhar for verification
+                    while (true) {
+                        System.out.println("0. Deny Request");
+                        System.out.println("1. Hold Request");
+                        System.out.println("2. Accept Request");
+                        System.out.println("3. Return to Main Request Menu");
+                        int requesting_choice = sc3.nextInt();
+                        switch (requesting_choice) {
+                            case 0:
+                                System.out.println("Enter any message for customer. Leave Blank for none!");
+                                String manager_msg = stringsc3.nextLine();
+                                Customer.temp.query = manager_msg + "Can't change username to " + Customer.temp.query;
+                                Customer.temp.request = 0;// Denied
+                                break;
+                            case 1:
+                                System.out.println("Holding Request to Address Later!");
+                                Customer.temp.request = 1;// Hold Request
+                                break;
+                            case 2:
+                                System.out.println("Request Accepted!");
+                                Customer.temp.request = 2;// Accept Request
+                                Customer.temp.username = Customer.temp.query;
+                                break;
+                            case 3:
+                                System.out.println("Returning to Menu");
+                                broker = 1;
+                                break;
+                        }
+                        if (broker == 1) {
+                            broker = 0;
+                            break;
+                        }
+                    }
+                    break;
+                case 3:
+                    broker = 1;
+                    break;
+                default:
+                    System.out.println("Wrong Choice! Please ReEnter");
+                    break;
+            }
+            if (broker == 1) {
+                broker = 0;
+                break;
+            }
+        }
+        isInSupport = false;// reassigns flag
     }
 
 }
